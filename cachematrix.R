318373d4ef9301581a0a27a7c9dbf1e207e32545
SasnakR <158030353+SasnakR@users.noreply.github.com>
2024-10-02 23:06:43 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,74 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+
+
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+}
+
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL  # Initialize the inverse property
+  
+  # Method to set the matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL  # Reset the inverse when the matrix is changed
+  }
+  
+  # Method to get the matrix
+  get <- function() x
+  
+  # Method to set the inverse of the matrix
+  setInverse <- function(inverse) inv <<- inverse
+  
+  # Method to get the inverse of the matrix
+  getInverse <- function() inv
+  
+  # Return a list of the methods
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+
+# Create a matrix
+matrix1 <- matrix(c(1, 2, 3, 4), 2, 2)
+
+# Create a special matrix object
+specialMatrix <- makeCacheMatrix(matrix1)
+
+# Compute the inverse, caching it
+inv1 <- cacheSolve(specialMatrix)
+
+# Retrieve the cached inverse without recalculating
+inv2 <- cacheSolve(specialMatrix)
+
+matrix()
+matrix1
+inv1
